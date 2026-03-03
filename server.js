4d92a50d171b6404e501ee150a1c7ef357d326d6
Hareesh <edigahareesh538@gmail.com>
2026-03-04 02:53:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -103,7 +103,9 @@ app.delete("/participants", async (req, res) => {
     }
 });
 console.log(process.env.MONGO_URI);
+PORT=process,env.PORT || 5000;
 
-app.listen(5000,"0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log("Server running on port 5000");
+
 });
