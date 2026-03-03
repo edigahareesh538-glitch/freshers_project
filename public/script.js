f8b4beddea30c89287346594f92dbe0e9a537ba1
Hareesh <edigahareesh538@gmail.com>
2026-03-04 02:34:05 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("register_form");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const roll = document.getElementById("roll").value;
+    const branch = document.getElementById("branch").value;
+    const phone = document.getElementById("phone").value;
+    const type = document.getElementById("type").value;
+
+    const response = await fetch("https://freshers-project.onrender.com:5000/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name,roll,branch, phone ,type})
+    });
+
+    const data = await response.json();
+    alert("Registration Successfully")
+});
